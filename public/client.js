f8f81cd3fb1a8c397573cc4980fd8d50a00159f5
wojtek <fraczak@gmail.com>
2023-03-27 22:28:07 -0400

    presentation

--- public/client.js
+++ public/client.js
@@ -1,41 +1,46 @@
-// using 'forge.min.js' from npm 'node-forge' package
+// using variable 'forge' from 'forge.min.js'
 
-// var keyPair = forge.pki.rsa.generateKeyPair( { bits: 1024 } );
+var keyPair;
 
-var getKeyPair = () => {
-  // generer et sauvegarder la clef privee en format PEM dans localStorage
-  // si elle n'y est pas encore...
-  var keyPair, pem = localStorage.getItem("pem");
-  if (pem) {
-    privateKey = forge.pki.privateKeyFromPem(pem);
-    publicKey = forge.pki.setRsaPublicKey(privateKey.n, privateKey.e);
-    keyPair = {privateKey, publicKey};
-  } else {
-    keyPair = forge.pki.rsa.generateKeyPair({bits: 1024});
-    localStorage.setItem("pem",forge.pki.privateKeyToPem(keyPair.privateKey));
-  };
-  return keyPair;
-};
+if (localStorage.getItem("pem")) {
+  const privateKey = forge.pki.privateKeyFromPem(localStorage.getItem("pem"));
+  const publicKey = forge.pki.setRsaPublicKey(privateKey.n, privateKey.e);
+  keyPair = { privateKey, publicKey };
+} else {
+  keyPair = forge.pki.rsa.generateKeyPair({ bits: 1024 });
+  localStorage.setItem("pem", forge.pki.privateKeyToPem(keyPair.privateKey));
+}
 
-var keyPair = getKeyPair();
+const encrypt = (msg) =>
+  forge.util.encode64(keyPair.publicKey.encrypt(forge.util.encodeUtf8(msg)));
 
-document.getElementById( "encrypt" ).addEventListener( "click", event => {
-  var msg = document.getElementById( "msg" ).value;
+const decrypt = (code) =>
+  forge.util.decodeUtf8(keyPair.privateKey.decrypt(forge.util.decode64(code)));
+
+document.getElementById("encrypt").addEventListener("click", (event) => {
   try {
-    document.querySelector( ".result" ).innerText =
-      forge.util.encode64( keyPair.publicKey.encrypt( forge.util.encodeUtf8( msg )));
+    document.querySelector(".result").innerText = encrypt(
+      document.getElementById("msg").value
+    );
   } catch (err) {
-    document.querySelector( ".result" ).innerText = err;
-  };
+    document.querySelector(".result").innerText = err;
+  }
 });
 
-document.getElementById( "decrypt" ).addEventListener( "click", event => {
-  var msg = document.getElementById( "msg" ).value;
+document.getElementById("decrypt").addEventListener("click", (event) => {
   try {
-    document.querySelector( ".result" ).innerText =
-      forge.util.decodeUtf8( keyPair.privateKey.decrypt( forge.util.decode64( msg )));
+    document.querySelector(".result").innerText = decrypt(
+      document.getElementById("msg").value
+    );
   } catch (err) {
-    document.querySelector( ".result" ).innerText = err;
-  };
+    document.querySelector(".result").innerText = err;
+  }
 });
 
+//--------------------------------------------------------------
+
+const printPublicKey = () =>
+  forge.pki.publicKeyToPem(keyPair.publicKey);
+
+const readPublicKey = (pem) =>
+  forge.pki.publicKeyFromPem(pem);
